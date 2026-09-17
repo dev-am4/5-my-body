52f9994c6cf43f3168eca85c0e8336654473297e
dev-am4 <am4.production29@gmail.com>
2026-09-17 09:17:06 +0700

    Load cinematic projection style fixes

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import '@fontsource/chakra-petch/600.css'
 import '@fontsource/chakra-petch/700.css'
 import App from './App'
 import './styles.css'
+import './cinema-fixes.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
