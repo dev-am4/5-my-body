c9e72b9dc4fdbbc30829905f8031b3dc725fb6cc
dev-am4 <am4.production29@gmail.com>
2026-09-17 09:20:39 +0700

    Apply reachable touch-zone layout

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import '@fontsource/chakra-petch/700.css'
 import App from './App'
 import './styles.css'
 import './cinema-fixes.css'
+import './touch-zone.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
